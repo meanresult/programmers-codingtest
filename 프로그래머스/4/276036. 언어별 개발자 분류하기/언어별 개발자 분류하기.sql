2c073076cad6dc0af8f200dec1695489d3a8d6d8
jeehun han <jeehunhan0420@gmail.com>
2026-05-23 03:00:28 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,34 +1,27 @@
--- 코드를 작성해주세요
+# SELECT      *
+# FROM        SKILLCODES AS S INNER JOIN DEVELOPERS AS D
+# ON          S.CODE & D.SKILL_CODE = S.CODE
+# ORDER BY    D.ID
+   
 
-# SELECT      CASE
-#                 WHEN S.NAME = 'Python' AND S.CATEGORY = 'Front End' THEN 'A'
-#                 WHEN S.NAME = 'C#'                                  THEN 'B'
-#                 WHEN S.CATEGORY = 'Front End'                       THEN 'C'
-#             END AS GRADE,
 
-WITH TEMP AS (
             
-                SELECT      CASE
-                                WHEN MAX(CASE WHEN S.NAME = 'Python' THEN 1 ELSE 0 END) = 1 
-                                 AND MAX(CASE WHEN S.CATEGORY = 'Front End' THEN 1 ELSE 0 END) = 1 
-                                THEN 'A'
+SELECT      CASE
+                WHEN MAX(CASE WHEN S.NAME = 'Python' THEN 1 ELSE 0 END) = 1 -- MAX가 없으면 동일한 ID의 여러 행 중에서 어떤 걸 가져와서 보여줘야할지 모름, 집계함수로 계산
+                 AND MAX(CASE WHEN S.CATEGORY = 'Front End' THEN 1 ELSE 0 END) = 1 
+                THEN 'A'
 
-                                WHEN MAX(CASE WHEN S.NAME = 'C#' THEN 1 ELSE 0 END) = 1 
-                                THEN 'B'
+                WHEN MAX(CASE WHEN S.NAME = 'C#' THEN 1 ELSE 0 END) = 1 
+                THEN 'B'
 
-                                WHEN MAX(CASE WHEN S.CATEGORY = 'Front End' THEN 1 ELSE 0 END) = 1 
-                                THEN 'C'
-                            END AS GRADE
-                            ,D.ID
-                            ,D.EMAIL
+                WHEN MAX(CASE WHEN S.CATEGORY = 'Front End' THEN 1 ELSE 0 END) = 1 
+                THEN 'C'
+            END AS GRADE
+            ,D.ID
+            ,D.EMAIL
 
-                FROM        SKILLCODES AS S INNER JOIN DEVELOPERS AS D
-                ON          S.CODE & D.SKILL_CODE = S.CODE
-                GROUP BY    D.ID, D.EMAIL
-
-            )
-
-SELECT      *
-FROM        TEMP
-WHERE       GRADE IS NOT NULL
+FROM        SKILLCODES AS S INNER JOIN DEVELOPERS AS D
+ON          S.CODE & D.SKILL_CODE = S.CODE
+GROUP BY    D.ID, D.EMAIL
+HAVING      GRADE IS NOT NULL
 ORDER BY    GRADE ASC, ID ASC
