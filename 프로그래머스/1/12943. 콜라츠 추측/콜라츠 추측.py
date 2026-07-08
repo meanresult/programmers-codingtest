c5e9f11ba694770d1fc682ca70fdd7fc6ea33e19
jeehun han <jeehunhan0420@gmail.com>
2026-07-08 18:59:45 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,13 @@
+def solution(num):
+    cnt = 0
+    
+    while cnt < 500 and num != 1 :
+        
+        if num % 2 == 0:
+            num //= 2
+        else :
+            num = num * 3 + 1
+            
+        cnt += 1 
+        
+    return cnt if num == 1 else -1
