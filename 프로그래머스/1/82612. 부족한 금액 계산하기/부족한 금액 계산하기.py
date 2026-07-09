77758cffa5a784691f57eb2c98c93918076940f8
jeehun han <jeehunhan0420@gmail.com>
2026-07-09 13:36:32 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.12 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -1,5 +1,5 @@
 def solution(price, money, count):
-    ps = sum(i*price for i in range(1,count+1)) - money
+    ps = sum(i*price for i in range(1,count+1)) - money # n번 타고 싶은 기구비용의 합 - 예산 = 모자른 비용 
     
     return 0 if ps < 0 else ps 
     
